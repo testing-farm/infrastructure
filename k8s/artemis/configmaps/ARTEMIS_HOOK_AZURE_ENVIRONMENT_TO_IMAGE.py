b3f9a8038bd36d98047f324db81b29c32306eae7
Miroslav Vadkerti <mvadkert@redhat.com>
2021-09-04 09:14:17 +0200

    Prepare for handover

---
+++ k8s/artemis/configmaps/ARTEMIS_HOOK_AZURE_ENVIRONMENT_TO_IMAGE.py
@@ -0,0 +1,15 @@
+import gluetool.glue
+import gluetool.log
+from gluetool.result import Result
+
+from tft.artemis import Failure
+from tft.artemis.drivers import PoolImageInfo
+from tft.artemis.drivers.azure import AzureDriver
+from tft.artemis.drivers.hooks import map_environment_to_image_info
+from tft.artemis.environment import Environment
+
+
+def hook_AZURE_ENVIRONMENT_TO_IMAGE(
+    *, logger: gluetool.log.ContextAdapter, pool: AzureDriver, environment: Environment,
+) -> Result[PoolImageInfo, Failure]:
+    return map_environment_to_image_info(logger, pool, environment, mapping_filename='artemis-image-map-azure.yaml')
---
+++ ranch/public/jobs/tf-sti.nomad
@@ -0,0 +1,38 @@
+job "tf-sti" {
+  type        = "batch"
+  datacenters = ["dc1"]
+
+  parameterized {
+      meta_required = ["REQUEST_ID"]
+  }
+
+  group "sti" {
+
+    # Restart up to 2 times
+    restart {
+      attempts = 2
+    }
+
+    reschedule {
+      attempts = 2
+    }
+
+    ephemeral_disk {
+      size = "5000"
+    }
+
+    task "sti" {
+      driver = "raw_exec"
+
+      resources {
+        cpu    = 500
+        memory = 512
+      }
+
+      config {
+        command = "tf-sti"
+        args = ["${NOMAD_META_REQUEST_ID}", "${NOMAD_ALLOC_DIR}"]
+      }
+    }
+  }
+}
