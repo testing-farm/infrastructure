4c07758eced95f6a6923fac89c736a591b828acb
Guy Inger <ginger@redhat.com>
2021-12-21 12:59:51 +0000

    Add frinedly cname record to the eks cluster installation via Terraforn

--- terraform/modules/eks/eks.tf
+++ terraform/modules/eks/eks.tf
@@ -7,8 +7,24 @@ resource "aws_eks_cluster" "testing_farm" {
   }
 }
 
+data "aws_route53_zone" "testing_farm_zone" {
+  name         = "testing-farm.io"
+}
+
+resource "aws_route53_record" "eks-friendly-endpoint" {
+  zone_id = data.aws_route53_zone.testing_farm_zone.zone_id
+  name    = "https://api.${var.cluster_name}.eks.${data.aws_route53_zone.testing_farm_zone.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [trimprefix(aws_eks_cluster.testing_farm.endpoint, "https://")]
+}
+
 output "eks-endpoint" {
   value = aws_eks_cluster.testing_farm.endpoint
+}
+
+output "eks-friendly-endpoint" {
+  value = aws_route53_record.eks-friendly-endpoint.name
 }
 
 output "eks-cluster-name" {
