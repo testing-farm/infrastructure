c697380d6e1ec2b6d6c3d2359b21dc6eceb24a1e
Daniel Simko <dasimko@redhat.com>
2022-08-16 18:10:23 +0200

    Update hooks to keep in line with updated syntax and semantics of Artemis codebase

--- terraform/environments/dev/config/ARTEMIS_HOOK_BEAKER_ENVIRONMENT_TO_IMAGE.py
+++ terraform/environments/dev/config/ARTEMIS_HOOK_BEAKER_ENVIRONMENT_TO_IMAGE.py
@@ -2,8 +2,7 @@ import gluetool.glue
 import gluetool.log
 from gluetool.result import Result
 
-from tft.artemis import Failure
-from tft.artemis.drivers import PoolImageInfo
+from tft.artemis.drivers import ImageInfoMapperOptionalResultType, PoolImageInfo
 from tft.artemis.drivers.beaker import BeakerDriver
 from tft.artemis.drivers.hooks import map_environment_to_image_info
 from tft.artemis.environment import Environment
@@ -11,5 +10,11 @@ from tft.artemis.environment import Environment
 
 def hook_BEAKER_ENVIRONMENT_TO_IMAGE(
 	*, logger: gluetool.log.ContextAdapter, pool: BeakerDriver, environment: Environment,
-) -> Result[PoolImageInfo, Failure]:
-	return map_environment_to_image_info(logger, pool, environment, mapping_filename='artemis-image-map-beaker.yml')
+) -> ImageInfoMapperOptionalResultType[PoolImageInfo]:
+	return map_environment_to_image_info(
+        logger,
+        pool,
+        environment,
+        '{{ os.compose }}',
+        mapping_filename='artemis-image-map-beaker.yml'
+    )
--- terraform/environments/dev/config/ARTEMIS_HOOK_OPENSTACK_ENVIRONMENT_TO_IMAGE.py
+++ terraform/environments/dev/config/ARTEMIS_HOOK_OPENSTACK_ENVIRONMENT_TO_IMAGE.py
@@ -1,9 +1,7 @@
 import gluetool.glue
 import gluetool.log
-from gluetool.result import Result
 
-from tft.artemis import Failure
-from tft.artemis.drivers import PoolImageInfo
+from tft.artemis.drivers import ImageInfoMapperOptionalResultType, PoolImageInfo
 from tft.artemis.drivers.hooks import map_environment_to_image_info
 from tft.artemis.drivers.openstack import OpenStackDriver
 from tft.artemis.environment import Environment
@@ -11,5 +9,11 @@ from tft.artemis.environment import Environment
 
 def hook_OPENSTACK_ENVIRONMENT_TO_IMAGE(
 	*, logger: gluetool.log.ContextAdapter, pool: OpenStackDriver, environment: Environment,
-) -> Result[PoolImageInfo, Failure]:
-	return map_environment_to_image_info(logger, pool, environment, mapping_filename='artemis-image-map-openstack.yml')
+) -> ImageInfoMapperOptionalResultType[PoolImageInfo]:
+	return map_environment_to_image_info(
+        logger,
+        pool,
+        environment,
+        '{{ os.compose }}',
+        mapping_filename='artemis-image-map-openstack.yml'
+    )
