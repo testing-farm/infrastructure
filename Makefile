0ce030a0ad3571f55420cbec759a5956c1e10087
Jan Havlin <jhavlin@redhat.com>
2023-03-03 15:37:39 +0100

    add environment.yaml.j2 to dev `citool-config`s

--- Makefile
+++ Makefile
@@ -1,3 +1,22 @@
-clean:
+.DEFAULT_GOAL := help
+
+DEV_ENVIRONMENT_FILES := terraform/environments/dev/ranch/public/citool-config/environment.yaml \
+                         terraform/environments/dev/ranch/redhat/citool-config/environment.yaml
+
+##@ Utility
+
+$(DEV_ENVIRONMENT_FILES):
+	poetry run python setup/generate_environment.py
+
+generate-environment-variables:  ## Generate `environment.yaml` files used in each citool configuration.
+	poetry run python setup/generate_environment.py
+
+clean:  ## Cleanup
 	rm -rf $$DIRENV_PATH
 	rm -rf $$VIRTUAL_ENV
+
+# See https://www.thapaliya.com/en/writings/well-documented-makefiles/ for details.
+reverse = $(if $(1),$(call reverse,$(wordlist 2,$(words $(1)),$(1)))) $(firstword $(1))
+
+help:  ## Show this help
+	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make [target]\033[36m\033[0m\n"} /^[a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-30s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(call reverse, $(MAKEFILE_LIST))
---
+++ setup/generate_environment.py
@@ -0,0 +1,45 @@
+# This script is used to generate `environment.yaml` files for each `citool-config` inside the dev environment. These
+# `environment.yaml` files are consumed by gluetool and contain credentials and values specific to each dev environment.
+# This script can be executed using `make`.
+
+import os
+import ruamel.yaml
+
+from ansible.constants import DEFAULT_VAULT_ID_MATCH
+from ansible.parsing.vault import VaultLib, VaultSecret
+from jinja2 import Template
+
+from typing import Union
+
+
+SecretsType = dict[str, Union[str, 'SecretsType']]
+
+
+def main() -> None:
+    with open('.vault_pass', 'r') as f:
+        vault_pass = f.read().strip()
+
+    with open(os.path.join('secrets', 'credentials.yaml'), 'r') as f:
+        credentials_encrypted = f.read()
+
+    vault = VaultLib([(DEFAULT_VAULT_ID_MATCH, VaultSecret(vault_pass.encode()))])
+    credentials_decrypted: SecretsType = ruamel.yaml.safe_load(vault.decrypt(credentials_encrypted))
+
+    for ranch in ['redhat', 'public']:
+        template_dirpath = os.path.join('terraform', 'environments', 'dev', 'ranch', ranch, 'citool-config')
+        template_filepath = os.path.join(template_dirpath, 'environment.yaml.j2')
+        result_template_filepath = os.path.join(template_dirpath, 'environment.yaml')
+
+        print('Generating `{}`...'.format(result_template_filepath))
+
+        with open(template_filepath, 'r') as f:
+            template = f.read()
+
+        template_rendered = Template(template).render({**credentials_decrypted, **dict(os.environ)})
+
+        with open(result_template_filepath, 'w') as f:
+            print(template_rendered, file=f)
+
+
+if __name__ == '__main__':
+    main()
