fdd4ff9fa7d93fbc7a37a90d35438a46032f77b7
Simon Walter <siwalter@redhat.com>
2025-05-14 09:10:37 +0000

    do not allow push to default branch in gitlab

--- terragrunt/modules/gitlab/project/main.tf
+++ terragrunt/modules/gitlab/project/main.tf
@@ -18,7 +18,7 @@ resource "gitlab_project" "project" {
   description                                      = var.description
   namespace_id                                     = var.namespace_id
   visibility_level                                 = "public"
-  default_branch                                   = "main"
+  default_branch                                   = var.default_branch
   approvals_before_merge                           = 2
   merge_method                                     = "ff"
   remove_source_branch_after_merge                 = true
@@ -27,4 +27,12 @@ resource "gitlab_project" "project" {
   only_allow_merge_if_all_discussions_are_resolved = true
   build_git_strategy                               = "fetch"
   initialize_with_readme                           = true
+}
+
+resource "gitlab_branch_protection" "default_branch_protection" {
+  project                = gitlab_project.project.id
+  branch                 = var.default_branch
+  push_access_level      = "no one"
+  merge_access_level     = "maintainer"
+  unprotect_access_level = "maintainer"
 }
