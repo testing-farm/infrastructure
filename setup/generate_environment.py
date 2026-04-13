5927ec1690c8456b417837288162e5203916435d
Jan Havlin <jhavlin@redhat.com>
2026-04-13 17:04:34 +0200

    Add support for citool-config image in tests

--- setup/generate_environment.py
+++ setup/generate_environment.py
@@ -1,6 +1,10 @@
 # This script is used to generate `environment.yaml` files for each `citool-config` inside the dev environment. These
 # `environment.yaml` files are consumed by gluetool and contain credentials and values specific to each dev environment.
 # This script can be executed using `make`.
+#
+# For the citool-config container image workflow, generated files are written to the .generated/ directory,
+# which contains a selection of citool-config files for local execution used in CI as well as credentials
+# and environment-specific values.
 
 import os
 import stat
@@ -100,9 +104,13 @@ def main() -> None:
             'staging_ci_suffix': f"-{os.getenv('STAGING_CI_SUFFIX')}" if os.getenv('STAGING_CI_SUFFIX') else ''
         })
 
-    template_dirpath = os.path.join('terragrunt', 'environments', environment, WORKER, 'citool-config')
-    template_filepath = os.path.join(template_dirpath, 'environment.yaml.j2')
-    result_template_filepath = os.path.join(template_dirpath, 'environment.yaml')
+    # Create .generated directory with secrets used for citool-config container image tests
+    source_dirpath = os.path.join('terragrunt', 'environments', environment, WORKER, 'citool-config')
+    output_dirpath = os.path.join('.generated', environment, WORKER, 'citool-config')
+    os.makedirs(output_dirpath, exist_ok=True)
+
+    template_filepath = os.path.join(source_dirpath, 'environment.yaml.j2')
+    result_template_filepath = os.path.join(output_dirpath, 'environment.yaml')
 
     print('Generating "{}"'.format(result_template_filepath))
 
@@ -115,20 +123,42 @@ def main() -> None:
         print(template_rendered, file=f)
 
     worker_artemis_ssh_key = f'{TERRAGRUNT_ENV_DIR}/{environment}/{WORKER}/citool-config/id_rsa_artemis'
-    worker_artemis_ssh_key_decrypted = f'{worker_artemis_ssh_key}.decrypted'
+    worker_artemis_ssh_key_decrypted = os.path.join(output_dirpath, 'id_rsa_artemis.decrypted')
 
     print(f'Decrypting "{worker_artemis_ssh_key}"')
 
     with open(worker_artemis_ssh_key, 'r') as f:
         ssh_key_encrypted = f.read()
 
-    print(f'Writing "{worker_artemis_ssh_key}"')
+    print(f'Writing "{worker_artemis_ssh_key_decrypted}"')
 
     with open(worker_artemis_ssh_key_decrypted, 'wb') as f:
         f.write(vault.decrypt(ssh_key_encrypted))
 
     print(f'Setting permissions of "{worker_artemis_ssh_key_decrypted}" to 600')
     os.chmod(worker_artemis_ssh_key_decrypted, stat.S_IRUSR | stat.S_IWUSR)
+
+    # Generate secret config files for CONFIG-SECRETS mount
+    secrets_config_dir = os.path.join(output_dirpath, 'config')
+    os.makedirs(secrets_config_dir, exist_ok=True)
+
+    # Resolve the API key from credentials
+    # NOTE: dev local environment uses staging TF API
+    environment_credentials = environment
+    if environment == 'dev' and WORKER == 'worker-local':
+        environment_credentials = 'staging'
+    api_key = credentials_decrypted['credentials']['testing_farm'][environment_credentials]['public']['users']['worker']['token']
+
+    testing_farm_request_path = os.path.join(secrets_config_dir, 'testing-farm-request')
+    print(f'Generating "{testing_farm_request_path}"')
+    with open(testing_farm_request_path, 'w') as f:
+        f.write(f'[default]\napi-key = {api_key}\n')
+
+    artemis_config_path = os.path.join(secrets_config_dir, 'artemis')
+    print(f'Generating "{artemis_config_path}"')
+    with open(artemis_config_path, 'w') as f:
+        f.write('[default]\nssh-key = ${config_root}/id_rsa_artemis.decrypted\n')
+
 
 if __name__ == '__main__':
     main()
